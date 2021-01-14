b617d4fc0f5ba670c52b6aa49dadb5f108f9d416
Skyler Suzuki <skylersuzuk1@gmail.com>
2021-01-14 13:15:40 -0800

    Added category home view. Added category to landmark model. Added dictionary for categories to model data.

---
+++ Landmarks/Landmarks/Views/Categories/CategoryHome.swift
@@ -0,0 +1,30 @@
+//
+//  CategoryHome.swift
+//  Landmarks
+//
+//  Created by Lambda_School_Loaner_204 on 1/14/21.
+//
+
+import SwiftUI
+
+struct CategoryHome: View {
+    @EnvironmentObject var modelData: ModelData
+
+    var body: some View {
+        NavigationView {
+            List {
+                ForEach(modelData.categories.keys.sorted(), id: \.self) { key in
+                    Text(key)
+                }
+            }
+            .navigationTitle("Featured")
+        }
+    }
+}
+
+struct CategoryHome_Previews: PreviewProvider {
+    static var previews: some View {
+        CategoryHome()
+            .environmentObject(ModelData())
+    }
+}
