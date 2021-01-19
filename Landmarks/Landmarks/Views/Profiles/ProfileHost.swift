8340776b99dc4ad0b251f2218c1f5a8288c77f8b
Skyler Suzuki <skylersuzuk1@gmail.com>
2021-01-19 15:18:35 -0800

    Added an edit button to ProfileHost uiview.

--- Landmarks/Landmarks/Views/Profiles/ProfileHost.swift
+++ Landmarks/Landmarks/Views/Profiles/ProfileHost.swift
@@ -8,10 +8,16 @@
 import SwiftUI
 
 struct ProfileHost: View {
+    @Environment(\.editMode) var editMode
     @State private var draftProfile = Profile.default
 
     var body: some View {
         VStack(alignment: .leading, spacing: 20) {
+            HStack {
+                Spacer()
+                EditButton()
+            }
+            
             ProfileSummary(profile: draftProfile)
         }
         .padding()
@@ -21,5 +27,6 @@ struct ProfileHost: View {
 struct ProfileHost_Previews: PreviewProvider {
     static var previews: some View {
         ProfileHost()
+            .environmentObject(ModelData())
     }
 }
