496cca51a3b1c6485e85ca30e1a3e4959b203421
Skyler Suzuki <skylersuzuk1@gmail.com>
2021-01-22 12:56:24 -0800

    Added images to assets for feature landmarks. Added featurecard and page view swift ui card. Added a UIKit Pageviewcontroller.

---
+++ Landmarks/Landmarks/Views/PageView/PageView.swift
@@ -0,0 +1,25 @@
+//
+//  PageView.swift
+//  Landmarks
+//
+//  Created by Lambda_School_Loaner_204 on 1/22/21.
+//
+
+import SwiftUI
+
+struct PageView<Page: View>: View {
+    var pages: [Page]
+
+    var body: some View {
+        PageViewController(pages: pages)
+    }
+}
+
+struct PageView_Previews: PreviewProvider {
+    static var previews: some View {
+        PageView(pages: ModelData().features.map {
+            FeatureCard(landmark: $0)
+        })
+        .aspectRatio(3 / 2, contentMode: .fit)
+    }
+}
